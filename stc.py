fccadd9c928cfd9aee33a09981924cb872de064e
ycanerol <ycanerol@gmail.com>
2017-05-16 16:05:37 +0200

    Cosmetic changes to match PEP8

--- stc.py
+++ stc.py
@@ -8,20 +8,23 @@ Created on Tue May 16 13:28:17 2017
 Spike-triggered covariance
 """
 from datetime import datetime
-execution_timer=datetime.now()
+import numpy as np
+execution_timer = datetime.now()
 
-sta_temp=sta(spikes,stimulus,filter_length)
-def stc(spikes,stimulus,filter_length,sta_temp):
-    covariance=np.matrix(np.zeros((filter_length,filter_length)))
-    
-    for i in range(filter_length,total_frames):
-        if spikes[i]!=0:
-                 snippet=stimulus[i:i-filter_length:-1]
-                 # Snippets are inverted before being added
-                 snpta=np.matrix(snippet-sta_temp)
-                 covariance=covariance+(snpta.T*snpta)*spikes[i]
+sta_temp = sta(spikes, stimulus, filter_length)
+
+
+def stc(spikes, stimulus, filter_length, sta_temp):
+    covariance = np.matrix(np.zeros((filter_length, filter_length)))
+    for i in range(filter_length, total_frames):
+        if spikes[i] != 0:
+            snippet = stimulus[i:i-filter_length:-1]
+            # Snippets are inverted before being added
+            snpta = np.matrix(snippet-sta_temp)
+            covariance = covariance+(snpta.T*snpta)*spikes[i]
     return covariance/(sum(spikes)+filter_length-1)
 
-recovered_stc=stc(spikes,stimulus,filter_length,sta(spikes,stimulus,filter_length))
-runtime=str(datetime.now()-execution_timer).split('.')[0]
-print('Duration: {}'.format(runtime))    
+recovered_stc = stc(spikes, stimulus, filter_length,
+                    sta(spikes, stimulus, filter_length))
+runtime = str(datetime.now()-execution_timer).split('.')[0]
+print('Duration: {}'.format(runtime))
