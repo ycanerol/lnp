3254e66a944147a259860902e01028b2af619048
ycanerol <ycanerol@gmail.com>
2017-05-22 18:22:12 +0200

    STA works with real data, STC to be put in a function

--- lnp.py
+++ lnp.py
@@ -23,10 +23,10 @@ def sta(spikes, stimulus, filter_length, total_frames):
             # Snippets are inverted before being added
     sta_unscaled = snippets/sum(spikes)   # Normalize/scale the STA
     sta_scaled = sta_unscaled/np.sqrt(sum(np.power(sta_unscaled, 2)))
-    return sta_scaled, sta_unscaled # Unscaled might be needed for STC
+    return sta_scaled, sta_unscaled  # Unscaled might be needed for STC
 
 
-def log_nlt_recovery(spikes, filtered_recovery, bin_nr):
+def log_nlt_recovery(spikes, filtered_recovery, bin_nr, dt):
     logbins = np.logspace(0, np.log(30)/np.log(10), bin_nr)
     logbins = -logbins[::-1]+logbins
     logbindices = np.digitize(filtered_recovery, logbins)
@@ -64,4 +64,3 @@ def stc(spikes, stimulus, filter_length, total_frames, dt):
             snpta = np.array(snippet-sta_temp)[np.newaxis, :]
             covariance = covariance+np.dot(snpta.T, snpta)*spikes[i]
     return covariance/(sum(spikes)-1)
-
--- with_real_data.py
+++ with_real_data.py
@@ -7,8 +7,8 @@ Created on Mon May 22 12:51:21 2017
 
 Analysis of real data
 
-Importing lnp only works if the directory containing LNP functions is added to 
-the python path variable like the following. 
+Importing lnp only works if the directory containing LNP functions is added to
+the python path variable like the following.
 
 sys.path.append('/Users/ycan/Documents/official/gottingen/lab rotations
 /LR3 Gollisch/scripts/')
@@ -20,23 +20,62 @@ This only needs to be done once.
 import h5py
 import numpy as np
 import os, sys
-from lnp import *
+import lnp
+from collections import Counter
 
-stimulus_path = '/Users/ycan/Documents/official/gottingen/lab rotations/LR3 Gollisch/data/fff2h'
-frames_path = '/Users/ycan/Documents/official/gottingen/lab rotations/LR3 Gollisch/data/Experiments/Salamander/2014_01_21/frametimes/2_fff2blinks_frametimings.mat'
-spike_path = '/Users/ycan/Documents/official/gottingen/lab rotations/LR3 Gollisch/data/Experiments/Salamander/2014_01_21/rasters/1_SP_C101.txt'
+main_dir = '/Users/ycan/Documents/official/gottingen/lab rotations/LR3 Gollisch/data/'
+stimulus_path = 'fff2h'
+frames_path = 'Experiments/Salamander/2014_01_21/frametimes/2_fff2blinks_frametimings.mat'
+spike_path = 'Experiments/Salamander/2014_01_21/rasters/2_SP_C101.txt'
 
-f = h5py.File(frames_path, 'r')
-ftimes = np.array(f.get('ftimes'))
-f.close(); del f
-#%%
-stimulus_file = open(stimulus_path)
+f = h5py.File(main_dir+frames_path, 'r')
+ftimes = (np.array(f.get('ftimes'))/1000)[:, 0]
+dt = np.average(np.ediff1d(ftimes))
+# Average difference between two frames in miliseconds
+f.close()
+del f
+
+total_frames = ftimes.shape[0]
+filter_length = 20  # Specified in nr of frames
+
+stimulus_file = open(main_dir+stimulus_path)
 stimulus = np.array([float(line) for line in stimulus_file])
 stimulus_file.close()
 
-spike_file = open(spike_path)
-spikes = np.array([float(line) for line in spike_file])
+spike_file = open(main_dir+spike_path)
+spike_times = np.array([float(line) for line in spike_file])
 spike_file.close()
 
+spike_counts = Counter(np.digitize(spike_times, ftimes))
+spikes = np.zeros(total_frames)
+spikes = np.array([spike_counts[i] for i in range(total_frames)])
+# Bin spikes
 
+stimulus = stimulus[:total_frames]
 
+# Start the analysis
+
+sta = lnp.sta(spikes, stimulus, filter_length, total_frames)[0]
+# Use scaled STA
+
+generator = np.convolve(recovered_kernel, stimulus,
+                        mode='full')[:-filter_length+1]
+
+logbins_sta, spikecount_in_logbins_sta = lnp.q_nlt_recovery(spikes, generator,
+                                                            60, dt)
+
+plt.plot(sta)
+plt.show()
+plt.scatter(logbins_sta,spikecount_in_logbins_sta,s=6,alpha=.6)
+plt.show()
+
+recovered_stc = stc(spikes, stimulus, filter_length, total_frames)
+
+w, v = np.linalg.eig(recovered_stc)
+# column v[:,i] is the eigenvector corresponding to the eigenvalue w[i]
+sorted_eig = np.argsort(w)[::-1]
+w = w[sorted_eig]
+v = v[:, sorted_eig]
+
+eigen_indices=np.where(np.abs(w-1)>.05)[0]
+manual_eigen_indices = [0, -1]
