f0afa9a3b7b34cbda2f4c5b036c4f071cd432ce3
ycanerol <ycanerol@gmail.com>
2017-06-07 11:19:05 +0200

    Add plots to onoffindex.py and load filenames

--- onoffplotter.py
+++ onoffplotter.py
@@ -18,22 +18,37 @@ stim_type = 3
 
 files_f = []  # Full field flicker
 files_c = []  # Checkerflicker
+
 for i in allfiles:
     if i[-4:] == '.npz':
-        if False and i[0] == str(2):
+        if i[0] == str(2):
             files_f.append(i)
         elif i[0] == str(3):
             files_c.append(i)
+onoffindices_f = np.array([])
+onoffindices_c = np.array([])
 
-
-onoffindices_f=np.array([])
-onoffindices_c=np.array([])
-
+filenames_f = []
+filenames_c = []
 for i in files_f:
     f = np.load(main_dir+i)
     onoffindices_f = np.append(onoffindices_f, f['onoffindex'])
+    filenames_f.append(f['filename'])
 for i in files_c:
     f = np.load(main_dir+i)
     onoffindices_c = np.append(onoffindices_c, f['onoffindex'])
+    filenames_c.append(f['filename'])
 
-plt.hist(onoffindices_c,bins=np.linspace(-1,1,num=40))    
+# %%
+plt.hist(onoffindices_f, bins=np.linspace(-1, 1, num=40), alpha=.6)
+plt.hist(onoffindices_c, bins=np.linspace(-1, 1, num=40), alpha=.6)
+plt.legend(['Full field', 'Checkerflicker'])
+plt.title('Histogram of On Off indices')
+plt.show()
+plt.scatter(onoffindices_f, onoffindices_c)
+plt.plot(np.linspace(-1, 1), np.linspace(-1, 1), '--')
+plt.title('On-Off indices obtained from Full field vs Checkerflicker')
+plt.ylabel('Checkerflicker')
+plt.xlabel('Full field flicker')
+plt.axis('square')
+plt.show()
