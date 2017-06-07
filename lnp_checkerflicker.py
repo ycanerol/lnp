Bugfix for onoffindex, and fix calculation of covariance matrix
- lnp.stc was still projecting the STA out of snippets, leading to
erroneous results.
- fff_analysis included mutliple stc bins and spikes in bins, leading
to wrong onoffindex calculations, now fixed
- Add 'or equal' clause for division of nonlinearity to on and off
regions Bugfix for onoffindex, and fix calculation of covariance matrix

lnp_checkerflicker.py
@@ -124,7 +124,7 @@ def onoffindex(temporal_filter, bins, spikecount_in_bins):
         spikecount_in_bins = spikecount_in_bins[::-1]
     # integrate non-linearity bin size * fire rate
     on_ind = np.where(bins > 0)
-    off_ind = np.where(bins < 0)
+    off_ind = np.where(bins <= 0)
     r_on = np.trapz(spikecount_in_bins[on_ind], bins[on_ind])
     r_off = np.trapz(spikecount_in_bins[off_ind], bins[off_ind])
     onoffindex = (r_on-r_off)/(r_on+r_off)

onoffplotter.py
@@ -1,3 +1,4 @@
+
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
@@ -28,8 +29,9 @@ for i in allfiles:
 onoffindices_f = np.array([])
 onoffindices_c = np.array([])
 
-filenames_f = []
-filenames_c = []
+#files_f = ['2_SP_C6602.npz']  # Full field flicker
+#files_c = []
+
 for i in files_f:
     f = np.load(main_dir+i)
     onoffindices_f = np.append(onoffindices_f, f['onoffindex'])
@@ -40,11 +42,14 @@ for i in files_c:
     filenames_c.append(f['filename'])
 
 # %%
+plt.figure(figsize=(10,6))
 plt.hist(onoffindices_f, bins=np.linspace(-1, 1, num=40), alpha=.6)
 plt.hist(onoffindices_c, bins=np.linspace(-1, 1, num=40), alpha=.6)
 plt.legend(['Full field', 'Checkerflicker'])
 plt.title('Histogram of On Off indices')
 plt.show()
+
+plt.figure(figsize=(8,8))
 plt.scatter(onoffindices_f, onoffindices_c)
 plt.plot(np.linspace(-1, 1), np.linspace(-1, 1), '--')
 plt.title('On-Off indices obtained from Full field vs Checkerflicker')
@@ -52,3 +57,5 @@ plt.ylabel('Checkerflicker')
 plt.xlabel('Full field flicker')
 plt.axis('square')
 plt.show()
+
+